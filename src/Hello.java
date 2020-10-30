174d51f11a168cfea1404051b3229a4bb8b8d414
Mihai998 <72911659+Mihai998@users.noreply.github.com>
2020-10-30 11:50:10 +0100

    Prova build 2

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,10 @@
 
 public class Hello
 {	
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		String stringa="Hello World";
+		System.out.println(stringa);
 	}
 
 }
